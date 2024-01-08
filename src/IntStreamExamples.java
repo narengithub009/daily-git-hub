a56deb684431ee9e7df21f1833e13928584c86ab
narengithub009 <narender.kon09@gmail.com>
2024-01-08 17:02:44 +0530

    Branch1 commit

---
+++ src/IntStreamExamples.java
@@ -0,0 +1,33 @@
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+public class IntStreamExamples {
+    public static void main(String[] args) {
+        
+        for(int i=0;i<6;i++){
+            if(i%2==0){
+                System.out.println(i+" is even number ");
+            }
+        }
+
+        IntStream intStream=IntStream.of(1,2,3,4,5);
+        IntStream intStream2=IntStream.of(new int[]{1,2,3,4,5});
+        IntStream intStream3=IntStream.range(1, 6);
+        IntStream.range(0, 10).filter(i->i%2==0).forEach(System.out::println);
+
+        System.out.println("---------");
+        IntStream.range(1, 10)
+        .filter(n->n>2)
+        .filter(n->n%2==0)
+        .forEach(System.out::println);
+
+        System.out.println("---------");
+
+        int sum = IntStream.of(1,2,3,4,5).sum();
+        System.out.println("the sum is : "+sum);
+        System.out.println("---------");
+
+       OptionalInt int1 = IntStream.of(1,2,3,4,5,6).max();
+       System.out.println("max value is :"+int1.getAsInt());
+    }
+}
---
+++ src/OptionalIntExample.java
@@ -0,0 +1,14 @@
+import java.util.OptionalInt;
+
+public class OptionalIntExample {
+    public static void main(String[] args) {
+        
+        OptionalInt optionalInt=OptionalInt.of(0);
+
+        System.out.println(optionalInt.isPresent()); //will return true if value is present!!
+        System.out.println(optionalInt.getAsInt());//get returns the value
+        System.out.println(optionalInt.orElse(9));
+        optionalInt.ifPresent(System.out::println);
+
+    }
+}
