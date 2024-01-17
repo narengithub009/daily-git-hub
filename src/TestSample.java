b1ab5ee72f2712d5131a847b3ee0843ddaa72b3d
narengithub009 <narender.kon09@gmail.com>
2024-01-17 08:12:51 +0530

    17/01/2024

---
+++ src/TestSample.java
@@ -0,0 +1,10 @@
+public class TestSample {
+
+    private static int i=10;
+    private static String str="Naren";
+    public static void main(String[] args) {
+        
+        System.out.println(i+" "+str);
+
+    }
+}
