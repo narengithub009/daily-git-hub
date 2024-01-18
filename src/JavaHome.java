178800fe6fa908c5be26cf56980410e371e5d7b2
narengithub009 <narender.kon09@gmail.com>
2024-01-18 11:19:06 +0530

    18/01/2024

---
+++ src/JavaHome.java
@@ -0,0 +1,7 @@
+public class JavaHome {
+    public static void main(String[] args) {
+        
+        String message = "JAVA_HOME".transform(System.getenv()::get);
+        System.out.println(message);
+    }
+}
