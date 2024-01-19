f4dbfe24528c4160281e7aac85de99167a67fc8e
narengithub009 <narender.kon09@gmail.com>
2024-01-19 19:30:52 +0530

    19/01/2024

---
+++ src/StringCountExample.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class StringCountExample {
+    public static void main(String[] args) {
+        
+        String input = "iloveloveindia";
+
+        Map<String,Long> map= Arrays.stream(input.split("")).
+        collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+
+        System.out.println(map);
+
+       List<String> duplicateEle= Arrays.stream(input.split("")).
+        collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+        .entrySet().stream().filter(t->t.getValue()>1)
+        .map(Map.Entry::getKey)
+        .collect(Collectors.toList());
+
+        System.out.println(duplicateEle);
+    }
+}
