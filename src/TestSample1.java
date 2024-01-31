a27a0c7bd818f2d7d6eab9a2029aa35cfe0811e4
narengithub009 <narender.kon09@gmail.com>
2024-01-31 08:10:07 +0530

    31/01/2024

---
+++ src/TestSample1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class TestSample1 {
+    public static void main(String[] args) {
+        
+        String[] str={"hello","howareyou","imgood","whataboutyou"};
+
+        String string = Arrays.stream(str).reduce((w1,w2)->w1.length()>w2.length()?w1:w2).get();
+        System.out.println(string);
+        
+    }
+}
