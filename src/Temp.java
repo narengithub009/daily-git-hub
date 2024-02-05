0b1ba7dcff750cdac715f75c7671bd8e0494e08b
narengithub009 <narender.kon09@gmail.com>
2024-02-05 06:05:51 +0530

    05/02/2024

---
+++ src/Temp.java
@@ -0,0 +1,21 @@
+public class Temp {
+    
+    static{
+        System.out.println(1);
+    }
+    static{
+        System.out.println(3);
+    }
+    
+    static{
+        System.out.println(2);
+    }
+    static{
+        System.out.println(2);
+    }
+    
+    
+    public static void main(String[] args) {
+        
+    }
+}
