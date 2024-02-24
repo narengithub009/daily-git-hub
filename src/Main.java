12b594f63496086faf66f83eea53f408f6db8c04
narengithub009 <narender.kon09@gmail.com>
2024-02-24 20:03:02 +0530

    24-02-2024

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    private static List<Integer> asList;
+    private static List<Integer> collect;
+
+    public static void main(String[] args) {
+        asList = Arrays.asList(1, 2, 3, 3, 42, 44, 22, 22, 4, 4, 5);
+        collect = asList.stream().distinct().collect(Collectors.toList());
+        asList.stream().filter(s -> s % 2 == 0).distinct().forEach(System.out::println);
+        System.out.println(collect);
+    }
+}
+
+class Parent {
+
+    public void print() {
+        System.out.println("Parent");
+    }
+
+    public void dispaly() {
+        print();
+    }
+}
+
+class Child extends Parent {
+    public void print() {
+        System.out.println("Child");
+    }
+}
