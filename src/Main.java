adc73d2af3ac609d979753d3105cda6e1a0bccac
narengithub009 <narender.kon09@gmail.com>
2024-02-25 19:06:11 +0530

    25-02-2024

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,21 @@ public class Main {
 
     private static List<Integer> asList;
     private static List<Integer> collect;
+    private static List<String> asList2;
 
     public static void main(String[] args) {
         asList = Arrays.asList(1, 2, 3, 3, 42, 44, 22, 22, 4, 4, 5);
         collect = asList.stream().distinct().collect(Collectors.toList());
         asList.stream().filter(s -> s % 2 == 0).distinct().forEach(System.out::println);
         System.out.println(collect);
+
+        asList2 = Arrays.asList("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight");
+        String string = asList2.stream()
+                .skip(asList2.size() - 1)
+                .findFirst()
+                .get();
+
+        System.out.println(string);
     }
 }
 
