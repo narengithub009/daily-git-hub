0ab58b63271fc843c40e7c9ff258386d6203abf1
narengithub009 <narender.kon09@gmail.com>
2024-09-10 11:30:37 +0530

    "ádded some more programs"

--- src/HelloTest.java
+++ src/HelloTest.java
@@ -37,6 +37,19 @@ public class HelloTest {
 
         Optional<Object> ofNullable = Optional.ofNullable("Hello");
         System.out.println(ofNullable.orElse("Default value..."));
+
+        List<Employee1> listOfEmployees = Arrays.asList(new Employee1("Narender", 12, 120000),
+                new Employee1("Aswitha", 13, 123323),
+                new Employee1("Neeraja", 14, 12342));
+        List<Employee1> highSalaryEmp = listOfEmployees.stream()
+                .filter(emp -> emp.getSalary() > 12000).collect(Collectors.toList());
+
+        highSalaryEmp.forEach(emp -> System.out.println(emp.getName()));
+
+        List<Integer> numList = Arrays.asList(1, 2, 3, 4, 7);
+        Integer sum = numList.parallelStream().reduce(0, (a, b) -> a + b);
+        System.out.println(sum);
+
     }
 
 }
