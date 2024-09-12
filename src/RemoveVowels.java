6d01bec466cde11d0c88bdb3b7d4a5b93b40c775
narengithub009 <narender.kon09@gmail.com>
2024-09-12 08:52:32 +0530

    Streams examples...

--- src/RemoveVowels.java
+++ src/RemoveVowels.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class RemoveVowels {
 
@@ -16,9 +18,19 @@ public class RemoveVowels {
 
         List<String> names = Arrays.asList("Narender", "Anny", "Neeraja", "Aswitha");
 
+        String[] strings = { "Narender1", "Anny1", "Neeraja1", "Aswitha1", "Aswitha1" };
+        Integer[] integers = { 1, 2, 5, 67, 4, 3, 46, 35, 7, 4, 37, 7, 7, 7 };
         Collections.sort(names, (a, b) -> a.compareTo(b));
 
         names.forEach(System.out::println);
 
+        Stream<String> streamArray = Stream.of(strings).sorted().map(String::toUpperCase).distinct();
+        streamArray.forEach(System.out::println);
+
+        List<Integer> limitStream = Stream.of(integers).limit(5).collect(Collectors.toList());
+        System.out.println(limitStream);
+
+        List<Integer> skipStream = Stream.of(integers).skip(5).collect(Collectors.toList());
+        System.out.println(skipStream);
     }
 }
