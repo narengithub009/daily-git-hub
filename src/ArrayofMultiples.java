f3a9a4d91f342af3a056d7cb8e8e357257fd90c4
narengithub009 <narender.kon09@gmail.com>
2024-09-15 07:54:05 +0530

    modified the arrayofmul file

--- src/ArrayofMultiples.java
+++ src/ArrayofMultiples.java
@@ -1,23 +1,28 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class ArrayofMultiples {
 
     public static int[] arrayOfMultiples(int num, int length) {
-        int[] result = new int[length];
-        int temp = 0;
-        for (int i = 0; i < length; i++) {
-            temp = temp + num;
-            result[i] = temp;
-            // num = result[i];
-        }
+        /*
+         * int[] result = new int[length];
+         * int temp = 0;
+         * for (int i = 0; i < length; i++) {
+         * temp = temp + num;
+         * result[i] = temp;
+         * // num = result[i];
+         * }
+         * 
+         * System.out.println(Arrays.toString(result));
+         * // System.out.println(Arrays.toString(result));
+         * System.out.println("***************************");
+         * return result;
+         */
 
-        System.out.println(Arrays.toString(result));
-        // System.out.println(Arrays.toString(result));
-        System.out.println("***************************");
-        return result;
+        return IntStream.range(1, length + 1).map(n -> n * num).toArray();
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(ArrayofMultiples.arrayOfMultiples(17, 6)));
+        System.out.println(Arrays.toString(ArrayofMultiples.arrayOfMultiples(7, 5)));
     }
 }
