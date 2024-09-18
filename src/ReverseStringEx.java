e3192055901ea78fda6a2fcd7fa450b73334195f
narengithub009 <narender.kon09@gmail.com>
2024-09-18 13:20:38 +0530

    Java programs...

---
+++ src/ReverseStringEx.java
@@ -0,0 +1,15 @@
+public class ReverseStringEx {
+
+    public static String reverse(String str) {
+        if (str.isEmpty()) {
+            return str;
+        }
+        System.out.println(str.substring(1));
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        String input = "HelloHowareyou";
+        System.out.println(ReverseStringEx.reverse(input));
+    }
+}
