748e85f71ca518bc41d5b5f51808ceedb3c57a24
narengithub009 <91239876+narengithub009@users.noreply.github.com>
2024-09-21 11:47:02 +0530

    "Commiting changes to branch2" (#38)

---
+++ src/Calculator1.java
@@ -0,0 +1,27 @@
+public class Calculator1 {
+
+    public static int calculator(int a, char operator, int b) {
+
+        switch (operator) {
+            case '+':
+                return a + b;
+            case '*':
+                return a * b;
+            case '-':
+                return a - b;
+            case '/':
+                if (b == 0) {
+                    throw new IllegalArgumentException("Division by zero is not allowed...");
+                }
+                return a / b;
+            default:
+                throw new IllegalArgumentException("Invalid operator...");
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculator(12, '/', 0));
+    }
+}
