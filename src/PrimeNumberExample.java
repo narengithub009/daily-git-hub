23406e2ddf1e04ec8430808f5d7dde877f6f2153
narengithub009 <narender.kon09@gmail.com>
2024-10-01 08:00:00 +0530

    added some more programs 01/10/2024

---
+++ src/PrimeNumberExample.java
@@ -0,0 +1,17 @@
+public class PrimeNumberExample {
+
+    public static void main(String[] args) {
+        int n = 12;
+        System.out.println(isPrimeNumber(n) ? n + " is Prime number" : n + " is not prime number");
+    }
+
+    public static boolean isPrimeNumber(int num) {
+        System.out.println(Math.sqrt(num));
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
