1355e79e15865c22f2f87cc33854f1177011e4a3
narengithub009 <narender.kon09@gmail.com>
2024-10-04 08:04:18 +0530

    armstrong number 04/10/2024

---
+++ src/ArmstrongNumberExample.java
@@ -0,0 +1,26 @@
+public class ArmstrongNumberExample {
+
+    public static void main(String[] args) {
+        isArmstrongNumberOrNot(370);
+    }
+
+    public static void isArmstrongNumberOrNot(int number) {
+
+        // int number = 123;
+        int sum = 0;
+
+        int temp = number;
+
+        while (temp > 0) {
+            int rem = temp % 10;
+            sum += (rem * rem * rem);
+            temp = temp / 10;
+        }
+
+        if (number == sum) {
+            System.out.println(number + " is armstrong number");
+        } else {
+            System.out.println(number + " is not armstrong number");
+        }
+    }
+}
