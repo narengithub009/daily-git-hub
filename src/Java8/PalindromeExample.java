9c513a46f43d5df340b41070bd54f7e93b4b3c96
narengithub009 <narender.kon09@gmail.com>
2024-10-11 00:39:12 +0530

    hello

--- src/Java8/PalindromeExample.java
+++ src/Java8/PalindromeExample.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 public class PalindromeExample {
 
     public static void main(String[] args) {
+        System.out.println("Palindrom check");
         System.out.println(isPalindromOrNot("abba"));
     }
 
