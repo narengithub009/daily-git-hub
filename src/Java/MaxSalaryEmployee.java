80a33f92b26261ce564f12112888adfa01a57fb0
narengithub009 <narender.kon09@gmail.com>
2024-10-16 12:56:14 +0530

    somemore programs

--- src/Java/MaxSalaryEmployee.java
+++ src/Java/MaxSalaryEmployee.java
@@ -3,7 +3,9 @@ package Java;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class MaxSalaryEmployee {
 
@@ -23,5 +25,71 @@ public class MaxSalaryEmployee {
         } else {
             System.out.println("No Employees in the list...");
         }
+
+        Optional<Employee> minSalary = employees.stream()
+                .min(Comparator.comparing(Employee::getSalary));
+
+        if (minSalary.isPresent()) {
+            System.out.println("EMployee min salary is :" + minSalary.get());
+        } else {
+            System.out.println("Employee not found in this list");
+        }
+
+        // Employees salary >60000
+
+        List<Employee> listOfEmployees = employees.stream()
+                .filter(employee -> employee.getSalary() >= 60000)
+                .collect(Collectors.toList());
+        listOfEmployees.forEach(list -> System.out.println(list));
+
+        // find avg salary of all employees
+
+        double avgSalary = employees.stream()
+                .mapToDouble(employee -> employee.getSalary())
+                .average()
+                .orElse(0.0);
+
+        System.out.println("The avg salary of employees :" + avgSalary);
+
+        // Group employees by salary...
+
+        Map<String, List<Employee>> salaryDistribution = employees.stream()
+                .collect(Collectors.groupingBy(emp -> {
+                    if (emp.getSalary() < 60000)
+                        return "Low Salary";
+                    else if (emp.getSalary() <= 80000)
+                        return "Medium Salary";
+                    else
+                        return "High Salary";
+                }));
+        salaryDistribution.forEach((range, empList) -> System.out.println(range + " " + empList));
+
+        // Sort employees by name
+
+        List<Employee> sortedEmployeeNames = employees.stream()
+                .sorted(Comparator.comparing(Employee::getName))
+                .collect(Collectors.toList());
+        System.out.println(sortedEmployeeNames);
+
+        // Convert List of Employees to List of Employee Names
+
+        List<String> empNames = employees.stream()
+                .map(Employee::getName)
+                .collect(Collectors.toList());
+        System.out.println(empNames);
+
+        // Check if Any Employee Has a Salary Greater than 85,000
+
+        boolean salaryCheck = employees.stream()
+                .anyMatch(emp -> emp.getSalary() > 85000);
+
+        System.out.println(salaryCheck);
+        // Sum of All Salaries
+
+        double sum = employees.stream()
+                .mapToDouble(Employee::getSalary)
+                .sum();
+
+        System.out.println(sum);
     }
 }
