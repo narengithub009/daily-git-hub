8282a1db8ab446863f68fbd69da4a95d9878c815
narengithub009 <narender.kon09@gmail.com>
2024-10-29 22:09:55 +0530

    somemore programs

--- src/Java8/ListOfSTringsJoining.java
+++ src/Java8/ListOfSTringsJoining.java
@@ -11,5 +11,9 @@ public class ListOfSTringsJoining {
         String formingString = listOfStrings.stream()
                 .collect(Collectors.joining(",", "[", "]"));
         System.out.println(formingString);
+
+        List<Integer> numbers = Arrays.asList(2, 30, 43, 2342, 232, 212, 2332);
+        numbers.stream()
+                .filter(c -> c % 5 == 0).collect(Collectors.toList()).forEach(System.out::println);
     }
 }
