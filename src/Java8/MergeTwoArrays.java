6148cffebf91ac41ac2f6973e385e9aa909a56f8
narengithub009 <narender.kon09@gmail.com>
2024-10-30 07:36:50 +0530

    somemore programs

---
+++ src/Java8/MergeTwoArrays.java
@@ -0,0 +1,17 @@
+package Java8;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MergeTwoArrays {
+
+    public static void main(String[] args) {
+
+        int[] intarray1 = { 1, 2, 244, 53, 2 };
+        int[] intarray2 = { 1, 2, 24, 5443, 3 };
+
+        int[] concatArray = IntStream.concat(Arrays.stream(intarray1), Arrays.stream(intarray2)).sorted().toArray();
+        System.out.println(Arrays.toString(concatArray));
+
+    }
+}
