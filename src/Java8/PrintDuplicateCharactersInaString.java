46c20c1766beb693b1f4d60a2c30e1be23f88fa9
narengithub009 <narender.kon09@gmail.com>
2024-11-11 07:36:53 +0530

    somemore programs

---
+++ src/Java8/PrintDuplicateCharactersInaString.java
@@ -0,0 +1,19 @@
+package Java8;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class PrintDuplicateCharactersInaString {
+
+    public static void main(String[] args) {
+
+        String str = "Java Concept of the Day Day".replaceAll("\\s+", "").toLowerCase();
+        Set<String> set = new HashSet<>();
+        Set<String> duplicateCharacter = Arrays.stream(str.split(""))
+                .filter(ch -> !set.add(ch))
+                .collect(Collectors.toSet());
+        System.out.println(duplicateCharacter);
+    }
+}
