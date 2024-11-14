aadcf77d3fd1ed9363412e9d2f1126dadf890575
narengithub009 <narender.kon09@gmail.com>
2024-11-14 07:52:04 +0530

    somemore programs

---
+++ src/Java8/NonRepeatedCharacterInAString.java
@@ -0,0 +1,25 @@
+package Java8;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class NonRepeatedCharacterInAString {
+
+    public static void main(String[] args) {
+
+        String inputString = "Java Concept Of The Day".replaceAll("\\s+", "").toLowerCase();
+        LinkedHashMap<String, Long> mapStrings = Arrays.stream(inputString.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
+
+        String string = mapStrings.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == 1)
+                .map(entry -> entry.getKey())
+                .findFirst()
+                .get();
+        System.out.println(string);
+    }
+}
